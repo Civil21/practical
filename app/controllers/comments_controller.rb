137e8ff5d4dfe79306527f35908c4a28a8aaff7a
Civil21 <diusolexander@gmail.com>
2019-06-29 01:27:19 +0300

    update comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,25 +1,32 @@
 class CommentsController < ApplicationController
   def create
     @request = Request.find(params[:request_id])
+    params[:comment][:author] = if session[:admin]
+                                  'admin'
+                                else
+                                  @request.name
+                                end
     comment = @request.comments.build(comment_params)
-    render 'requests/show' if comment.save
+    if session[:token] == @request.token || session[:admin]
+      redirect_to request_path(@request.id) if comment.save
+    end
   end
 
   def update
-    comment.update(comment_params)
+    comment.update(comment_params) if session[:token] == @request.token
     render 'requests/show'
   end
 
   def destroy
     @request = Request.find(params[:request_id])
-    comment.destroy
+    comment.destroy if (session[:token] == @request.token && comment.author != 'admin') || (session[:admin] && comment.author == 'admin')
     redirect_to request_path(@request.id)
   end
 
   private
 
   def comment_params
-    params.require(:comment).permit(:autor, :text, :request_id)
+    params.require(:comment).permit(:author, :text, :request_id)
   end
 
   def comment
--- app/models/request.rb
+++ app/models/request.rb
@@ -1,3 +1,4 @@
 class Request < ApplicationRecord
   has_many :comments
+  validates :name, :text, :stat, :subject, presence: true
 end
